c627158d149fee86bbaff4618fdc1a7cb0df61f5
Mohamed Tijani Daoud <mohamed.daoud99@hotmail.com>
2023-12-18 18:33:17 +0100

    commit 5

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors =require('cors')
 
 app.use(cors(
     {
-        origin:["https://employee-management-nodejs-mohameddaoud99s-projects.vercel.app"],
+        origin:["https://employee-management-nodejs.vercel.app/"],
         methods :["POST","GET"],
         Credentials :true
     }
